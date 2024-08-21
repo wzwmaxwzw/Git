2a7888f8187060852878548c90ab731bea68bbc2
Max <3241558739@qq.com>
2024-08-21 19:18:07 +0800

    文件

--- src/main/java/thread/TicketRunnable.java
+++ src/main/java/thread/TicketRunnable.java
@@ -9,14 +9,12 @@ public class TicketRunnable implements Runnable{
     @Override
     public void run() {
         for(int i=0;i<100;i++){
-            synchronized (TicketRunnable.class){
-                if(ticketNum>0){
-                    System.out.println("我从"+Thread.currentThread().getName()+"买到了"+"第"+ticketNum--+"火车票");
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            if(ticketNum>0){
+                System.out.println("我从"+Thread.currentThread().getName()+"买到了"+"第"+ticketNum--+"火车票");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
